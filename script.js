3a8b8904cb6bff168e0f9db7818c792f65ec57ed
tannerwolff <tawolff13@gmail.com>
2026-06-23 11:25:51 -0700

    Add button choices instead of prompt for Human
    Add button choices for humans to choose from
    instead of having to type in an answer every time.
    Remove playGame() functionality for testing
    buttons so each one button click calls for a single
    round to be played.

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let humanScore = 0;
 
 function getHumanChoice() {
   let userInput = prompt("Rock, Paper or Scissors?");
-  return userInput.toLowerCase();
+  if (userInput !== null) {
+    return userInput.toLowerCase();
+  }
 }
 
 function getComputerChoice() {
@@ -18,43 +20,50 @@ function getComputerChoice() {
 }
 getComputerChoice();
 
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    function playRound(humanChoice, computerChoice) {
-      console.log(`Human: ${humanChoice}, Comp: ${computerChoice}`);
-      if (humanChoice === computerChoice) {
-        console.log("It's a tie");
-      } else if (humanChoice === "rock" && computerChoice === "paper") {
-        console.log("Computer wins");
-        ++computerScore;
-      } else if (humanChoice === "rock" && computerChoice === "scissors") {
-        console.log("Human wins");
-        ++humanScore;
-      } else if (humanChoice === "paper" && computerChoice === "rock") {
-        console.log("Human wins");
-        ++humanScore;
-      } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        console.log("Computer wins");
-        ++computerScore;
-      } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        console.log("Computer wins");
-        ++computerScore;
-      } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        console.log("Human wins");
-        ++humanScore;
-      } else {
-        alert("Please reload page and pick 'rock', 'paper', or 'scissors'");
-      }
-    }
-    playRound(getHumanChoice(), getComputerChoice());
-  }
-  console.log(`Human: ${humanScore}, Comp: ${computerScore}`);
-  if (humanScore > computerScore) {
-    console.log("Human Wins the Whole Game");
-  } else if (computerScore > humanScore) {
-    console.log("Computer Wins the Whole Game");
+function playRound(humanChoice, computerChoice) {
+  console.log(`Human: ${humanChoice}, Comp: ${computerChoice}`);
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie");
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("Computer wins");
+    ++computerScore;
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("Human wins");
+    ++humanScore;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("Human wins");
+    ++humanScore;
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log("Computer wins");
+    ++computerScore;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("Computer wins");
+    ++computerScore;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("Human wins");
+    ++humanScore;
   } else {
-    console.log("Game ends in a tie.");
+    alert("Please reload page and pick 'rock', 'paper', or 'scissors'");
   }
 }
-playGame();
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    playRound(button.id, getComputerChoice());
+  });
+});
+// function playGame() {
+//   for (let i = 0; i < 5; i++) {
+//     playRound(getHumanChoice(), getComputerChoice());
+//   }
+//   console.log(`Human: ${humanScore}, Comp: ${computerScore}`);
+//   if (humanScore > computerScore) {
+//     console.log("Human Wins the Whole Game");
+//   } else if (computerScore > humanScore) {
+//     console.log("Computer Wins the Whole Game");
+//   } else {
+//     console.log("Game ends in a tie.");
+//   }
+// }
+// playGame();
